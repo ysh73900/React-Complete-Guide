04748339d7c2b711121b6d07fbe91c1e4c176cec
ysh73900 <kim73900@naver.com>
2022-11-17 01:43:13 +0900

    예제1

---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,17 @@
+import "./ExpenseDate.css";
+
+function ExpenseDate(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" }); // 2-digit : 두 자리로 출력
+  const year = props.date.getFullYear(); // getFullYear : year을 4자리로 출력
+
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </div>
+  );
+}
+
+export default ExpenseDate;
